7d2af2c7d677a1fb7f43b0b75d797c9e18dfe364
Bogdan <bogdanfirchuk18@gmail.com>
2025-09-16 13:47:05 +0300

    Add coments

--- lab_7.py
+++ lab_7.py
@@ -1,20 +1,20 @@
-# Введення розміру матриці
+#Entering the matrix size
 n = int(input("Enter matrix size: "))
 symbol = input("Enter symbol: ")
 
-# Створюємо зубчатий масив (спочатку порожній)
+#Create a jagged array (initially empty)
 jagged = [[] for _ in range(n)]
 
-# Верхня частина — порожні рядки
+#Top part - empty lines
 for i in range(n // 2):
     jagged[i] = []
 
-# Нижня частина — формуємо трикутник
+# The lower part - we form a triangle
 for i in range(n // 2, n):
     row_length = 2 * (i - n // 2) + 1
     jagged[i] = [symbol] * row_length
 
-# Вивід трикутника по центру
+#Center triangle output
 for row in jagged:
     spaces = (n - len(row)) // 2
     print(" " * spaces + "".join(row))
